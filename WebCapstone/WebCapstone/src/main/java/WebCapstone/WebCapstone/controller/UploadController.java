8c131d0b45abd18f0ce428bd8737526d3b538ba4
luckyPrice <luckyprice1103@naver.com>
2023-03-19 00:46:47 +0900

    3/18 종합

--- WebCapstone/WebCapstone/src/main/java/WebCapstone/WebCapstone/controller/UploadController.java
+++ WebCapstone/WebCapstone/src/main/java/WebCapstone/WebCapstone/controller/UploadController.java
@@ -65,7 +65,6 @@ public class UploadController {
 
     @GetMapping("/UploadShow")// 업로드 기능
     public List<UploadDTO> ShowUpload(){
-        System.out.println(showUploadService.ShowUpload());
         return showUploadService.ShowUpload();
 
 
